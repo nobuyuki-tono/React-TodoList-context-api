0dfd758b393db5fd539404786a934709042dba4f
Nobu <rockmusic2090@gmail.com>
2020-02-23 12:15:43 -0800

    Added handleSubmit function

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,17 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+
+import GlobalContext from "../context/GlobalState";
 
 import "../styles/AddTask.css";
 
 const AddTask = () => {
   const [task, setTask] = useState("");
 
+  const { addTask } = useContext(GlobalContext);
+
   const handleChange = e => {
     setTask(e.target.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const newTask = {
+      id: Math.floor(Math.random() * 100000000),
+      text
+    };
+    addTask(newTask);
   };
   return (
     <>
       <h3>Add New Task</h3>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="form-controll">
           <label htmlFor="text">Task</label>
           <input
--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,5 +1,10 @@
 export default (state, action) => {
   switch (action.type) {
+    case "ADD_TASK":
+      return {
+        ...state,
+        tasks: [action.payload, ...state.tasks]
+      };
     case "DELETE_TASK":
       return {
         ...state,
